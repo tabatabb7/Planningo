30adffdbf15006a6a6bfc6f8a77755429942b6c0
tabatabb7 <yoosallyh@gmail.com>
2020-12-03 18:29:08 -0500

    WIP: Service Worker may be working

--- public/pwabuilder-sw.js
+++ public/pwabuilder-sw.js
@@ -1,10 +1,5 @@
-// This is the "Offline copy of assets" service worker
-
-const CACHE = "pwabuilder-offline";
-const QUEUE_NAME = "bgSyncQueue";
-
 importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
+  "https://storage.googleapis.com/workbox-cdn/releases/5.1.1/workbox-sw.js"
 );
 
 self.addEventListener("message", (event) => {
@@ -12,15 +7,3 @@ self.addEventListener("message", (event) => {
     self.skipWaiting();
   }
 });
-
-const bgSyncPlugin = new workbox.backgroundSync.Plugin(QUEUE_NAME, {
-  maxRetentionTime: 24 * 60, // Retry for max of 24 Hours (specified in minutes)
-});
-
-workbox.routing.registerRoute(
-  new RegExp("/*"),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE,
-    plugins: [bgSyncPlugin],
-  })
-);
