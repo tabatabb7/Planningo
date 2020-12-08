8550496164cf43a20eb74623d723753bfeccf8f5
Dana Zhang <danadaners@gmail.com>
2020-12-08 11:32:57 -0500

    bug fixed

--- server/api/tasks.js
+++ server/api/tasks.js
@@ -1,23 +1,34 @@
 const router = require("express").Router();
-const { Task, User_Task, Group, User, Task_Group, Shopping } = require("../db/models");
-const Op = require('sequelize').Op;
-
+const {
+  Task,
+  User_Task,
+  Group,
+  User,
+  Task_Group,
+  Shopping,
+} = require("../db/models");
+const Op = require("sequelize").Op;
 
 router.get("/", async (req, res, next) => {
   try {
     const user = await User.findByPk(req.user.id, {
-      include: [{
-        model: Group,
-      },
-      {
-        model: Task,
-        where: {
-                  shoppingId: null
-                },
-      }]
+      include: [
+        {
+          model: Group,
+        },
+        {
+          model: Task,
+          where: {
+            shoppingId: {
+              [Op.is]: null,
+            },
+          },
+          required: false,
+        },
+      ],
     });
     res.json(user);
-    console.log(user)
+    console.log(user);
   } catch (err) {
     next(err);
   }
@@ -27,31 +38,33 @@ router.get("/", async (req, res, next) => {
 router.get("/shopping", async (req, res, next) => {
   try {
     const user = await User.findByPk(req.user.id, {
-      include: [{
-        model: Group,
-      },
-      {
-        model: Task,
-        where: {
-          shoppingId: {
-            [Op.not]: null
-          }
-        }
-      }]
+      include: [
+        {
+          model: Group,
+        },
+        {
+          model: Task,
+          where: {
+            shoppingId: {
+              [Op.not]: null,
+            },
+          },
+          required: false,
+        },
+      ],
     });
     res.json(user);
-    console.log(user)
+    console.log(user);
   } catch (err) {
     next(err);
   }
 });
 
-
 router.get("/:taskId", async (req, res, next) => {
   try {
     const task = await Task.findOne({
       where: {
-        id: req.params.taskId
+        id: req.params.taskId,
       },
     });
     res.json(task);
@@ -60,19 +73,18 @@ router.get("/:taskId", async (req, res, next) => {
   }
 });
 
-
 router.post("/", async (req, res, next) => {
   try {
-    console.log(req.user)
+    console.log(req.user);
     const group = await Group.findOne({
       where: {
-        name: req.body.selected
-      }
-    })
+        name: req.body.selected,
+      },
+    });
     const task = await Task.create({
       userId: req.user.id,
       name: req.body.name,
-      shoppingId: null
+      shoppingId: null,
     });
     await User_Task.create({
       userId: req.user.id,
@@ -89,20 +101,19 @@ router.post("/", async (req, res, next) => {
   }
 });
 
-
 router.post("/shopping", async (req, res, next) => {
   try {
-    console.log(req.user)
+    console.log(req.user);
     const group = await Group.findOne({
       where: {
-        name: req.body.selected
-      }
-    })
-    const shopping = await Shopping.create(req.body)
+        name: req.body.selected,
+      },
+    });
+    const shopping = await Shopping.create(req.body);
     const task = await Task.create({
       userId: req.user.id,
       name: req.body.name,
-      shoppingId: shopping.id
+      shoppingId: shopping.id,
     });
     await User_Task.create({
       userId: req.user.id,
@@ -119,10 +130,9 @@ router.post("/shopping", async (req, res, next) => {
   }
 });
 
-
 router.put("/:taskId", async (req, res, next) => {
   try {
-    const task = await Task.findByPk(req.params.taskId)
+    const task = await Task.findByPk(req.params.taskId);
     task.update(req.body);
     res.json(task);
   } catch (err) {
@@ -133,22 +143,21 @@ router.put("/:taskId", async (req, res, next) => {
 //PATCH task
 router.patch("/:taskId", async (req, res, next) => {
   try {
-    const task = await Task.findByPk(req.params.taskId)
-    const {updatedFields} = req.body
-    task.update({...updatedFields})
-    res.sendStatus(204)
+    const task = await Task.findByPk(req.params.taskId);
+    const { updatedFields } = req.body;
+    task.update({ ...updatedFields });
+    res.sendStatus(204);
   } catch (err) {
     next(err);
   }
 });
 
-
 router.delete("/:taskId", async (req, res, next) => {
   try {
     await Task.destroy({
       where: {
-        id: req.params.taskId
-      }
+        id: req.params.taskId,
+      },
     });
     res.sendStatus(204);
   } catch (err) {
