e13994a53860ebb12e03e46227043fafa72f2856
Dana Zhang <danadaners@gmail.com>
2020-12-11 16:53:42 -0500

    fixing conflicts

--- client/custom-sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener("push", event => {
-  const data = event.data.json()
-  const { title } = data
-  const body = {
-      body: data.body,
-      icon: data.icon
-  }
-  event.waitUntil(self.registration.showNotification(title, body))
-})
