e121f65039d4134d77fbd28e51d24849d9dfa8f1
Dana Zhang <danadaners@gmail.com>
2020-12-13 21:20:19 -0500

    added calendar with date-fns instead of moment

--- client/components/Calendar/AppCalendar.js
+++ client/components/Calendar/AppCalendar.js
@@ -1,35 +1,164 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import Calendar from 'react-calendar';
-import { DateTime } from 'luxon';
-import './ReactCalendar.css'
+import React from "react";
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-
-export default function Sample() {
-  const [value, onChange, setMsg] = useState(new Date());
-  const date = DateTime.local()
-  const result = date.setZone('America/Los_Angeles')
-
-  console.log(date.toString())
-
-  console.log(result.toString())
-
-  return (
-    <div className="Sample">
-      <header>
-        <h1 className="calendar-title"></h1>
-
-      </header>
-      <div className="Sample__container">
-        <main className="Sample__container__content">
-          <Calendar
-            onChange={onChange}
-            value={value}
-          />
-        </main>
-      </div>
-    </div>
-  );
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import format from 'date-fns/format'
+import parse from 'date-fns/parse'
+import startOfWeek from 'date-fns/startOfWeek'
+import getDay from 'date-fns/getDay'
+import toDate from 'date-fns/toDate'
+const locales = {
+  'en-US': require('date-fns/locale/en-US'),
 }
-=======
->>>>>>> 9579eca9306bde2854b99511e9feb084cd65dcad
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+  class AppCalendar extends React.Component {
+    constructor() {
+      super();
+      const now = new Date();
+      const events = [
+        {
+            id: 0,
+            title: 'All Day Event very long title',
+            allDay: true,
+            start: new Date(2015, 3, 0),
+            end: new Date(2015, 3, 1),
+        },
+        {
+            id: 1,
+            title: 'Long Event',
+            start: new Date(2015, 3, 7),
+            end: new Date(2015, 3, 10),
+        },
+
+        {
+            id: 2,
+            title: 'DTS STARTS',
+            start: new Date(2016, 2, 13, 0, 0, 0),
+            end: new Date(2016, 2, 20, 0, 0, 0),
+        },
+
+        {
+            id: 3,
+            title: 'DTS ENDS',
+            start: new Date(2016, 10, 6, 0, 0, 0),
+            end: new Date(2016, 10, 13, 0, 0, 0),
+        },
+
+        {
+            id: 4,
+            title: 'Some Event',
+            start: new Date(2015, 3, 9, 0, 0, 0),
+            end: new Date(2015, 3, 10, 0, 0, 0),
+        },
+        {
+            id: 5,
+            title: 'Conference',
+            start: new Date(2015, 3, 11),
+            end: new Date(2015, 3, 13),
+            desc: 'Big conference for important people',
+        },
+        {
+            id: 6,
+            title: 'Meeting',
+            start: new Date(2015, 3, 12, 10, 30, 0, 0),
+            end: new Date(2015, 3, 12, 12, 30, 0, 0),
+            desc: 'Pre-meeting meeting, to prepare for the meeting',
+        },
+        {
+            id: 7,
+            title: 'Lunch',
+            start: new Date(2015, 3, 12, 12, 0, 0, 0),
+            end: new Date(2015, 3, 12, 13, 0, 0, 0),
+            desc: 'Power lunch',
+        },
+        {
+            id: 8,
+            title: 'Meeting',
+            start: new Date(2015, 3, 12, 14, 0, 0, 0),
+            end: new Date(2015, 3, 12, 15, 0, 0, 0),
+        },
+        {
+            id: 9,
+            title: 'Happy Hour',
+            start: new Date(2015, 3, 12, 17, 0, 0, 0),
+            end: new Date(2015, 3, 12, 17, 30, 0, 0),
+            desc: 'Most important meal of the day',
+        },
+        {
+            id: 10,
+            title: 'Dinner',
+            start: new Date(2015, 3, 12, 20, 0, 0, 0),
+            end: new Date(2015, 3, 12, 21, 0, 0, 0),
+        },
+        {
+            id: 11,
+            title: 'Birthday Party',
+            start: new Date(2015, 3, 13, 7, 0, 0),
+            end: new Date(2015, 3, 13, 10, 30, 0),
+        },
+        {
+            id: 12,
+            title: 'Late Night Event',
+            start: new Date(2015, 3, 17, 19, 30, 0),
+            end: new Date(2015, 3, 18, 2, 0, 0),
+        },
+        {
+            id: 12.5,
+            title: 'Late Same Night Event',
+            start: new Date(2015, 3, 17, 19, 30, 0),
+            end: new Date(2015, 3, 17, 23, 30, 0),
+        },
+        {
+            id: 13,
+            title: 'Multi-day Event',
+            start: new Date(2015, 3, 20, 19, 30, 0),
+            end: new Date(2015, 3, 22, 2, 0, 0),
+        },
+        {
+            id: 14,
+            title: 'Today',
+            start: new Date(new Date().setHours(new Date().getHours() - 3)),
+            end: new Date(new Date().setHours(new Date().getHours() + 3)),
+        },
+        {
+            id: 15,
+            title: 'Point in Time Event',
+            start: now,
+            end: now,
+        },
+      ]
+      this.state = {
+        name: 'React',
+        events
+      };
+    }
+
+    render() {
+
+      const date = toDate((new Date()))
+
+      return (
+        <div>
+          <div style={{ height: '500pt'}}>
+            <Calendar
+              events={this.state.events}
+              startAccessor="start"
+              endAccessor="end"
+              defaultDate={date}
+              localizer={localizer}
+            />
+
+          </div>
+        </div>
+      );
+    }
+  }
+
+  export default AppCalendar;
