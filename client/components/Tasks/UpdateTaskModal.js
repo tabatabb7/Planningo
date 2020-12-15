a3b5a3e53dc7801089980d560122559ff3bdd2f3
Dana Zhang <danadaners@gmail.com>
2020-12-15 03:08:37 -0500

    commiting recent css changes

--- client/components/Tasks/UpdateTaskModal.js
+++ client/components/Tasks/UpdateTaskModal.js
@@ -59,7 +59,7 @@ class UpdateTaskModal extends Component {
     }
     return (
       <div>
-        <div className="task-modal-content">
+        <div className="task-modal-content edit">
           <div id="top-taskmodal-div">
             <div id="modal-title">{this.props.task.name}</div>
             <button
@@ -76,7 +76,7 @@ class UpdateTaskModal extends Component {
               <input
                 name="name"
                 type="text"
-                className="modal-input name"
+                className="edit-modal-input name"
                 onChange={this.handleChange}
                 value={this.state.name}
               />
@@ -86,7 +86,7 @@ class UpdateTaskModal extends Component {
                 name="description"
                 type="text"
                 rows="4"
-                className="modal-input desc"
+                className="edit-modal-input desc"
                 onChange={this.handleChange}
                 value={this.state.description}
               />
@@ -95,7 +95,7 @@ class UpdateTaskModal extends Component {
               <textarea
                 name="points"
                 type="text"
-                className="modal-input points"
+                className="edit-modal-input points"
                 onChange={this.handleChange}
                 value={this.state.points}
               />
@@ -142,7 +142,7 @@ class UpdateTaskModal extends Component {
                     }
                     value={this.state.categoryId || ""}
                     name="categoryId"
-                    className="choose-category"
+                    className="edit-choose-category"
                   >
                     <option value="">None</option>
                     {categories
