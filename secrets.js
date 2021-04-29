70c78296e44ba951baa1c9af8a0dececeb1b8140
tabatabb7 <yoosallyh@gmail.com>
2021-04-29 11:44:05 -0400

    WIP: secrets file needs configuring

---
+++ secrets.js
@@ -0,0 +1,2 @@
+const dbURL =
+  "postgres://znncnmtnyxergd:bbe0dd5655788416de5a3336264ce726ac042fce862129b80be4d4fc84d5f425@ec2-34-225-167-77.compute-1.amazonaws.com:5432/d6b7n50tns7hms";
--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 const pkg = require("../../package.json");
-
+const dbURL = require("../../secrets");
 const databaseName =
   pkg.name + (process.env.NODE_ENV === "test" ? "-test" : "");
 
