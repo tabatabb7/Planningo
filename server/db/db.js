79cfd7050b386c4329b151ec496c46b7ece35baa
tabatabb7 <yoosallyh@gmail.com>
2021-04-29 11:09:56 -0400

    FIX: database URL

--- server/db/db.js
+++ server/db/db.js
@@ -5,9 +5,21 @@ const databaseName =
   pkg.name + (process.env.NODE_ENV === "test" ? "-test" : "");
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://znncnmtnyxergd:bbe0dd5655788416de5a3336264ce726ac042fce862129b80be4d4fc84d5f425@ec2-34-225-167-77.compute-1.amazonaws.com:5432/d6b7n50tns7hms`,
+  process.env.DATABASE_URL ||
+    "postgres://znncnmtnyxergd:bbe0dd5655788416de5a3336264ce726ac042fce862129b80be4d4fc84d5f425@ec2-34-225-167-77.compute-1.amazonaws.com:5432/d6b7n50tns7hms",
   {
-    logging: false,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: true,
+    },
+    // sequelize: {
+    //   uri:
+    //     "postgres://znncnmtnyxergd:bbe0dd5655788416de5a3336264ce726ac042fce862129b80be4d4fc84d5f425@ec2-34-225-167-77.compute-1.amazonaws.com:5432/d6b7n50tns7hms",
+    //   options: {
+    //     native: true,
+    //     ssl: true,
+    //   },
+    // },
   }
 );
 module.exports = db;
