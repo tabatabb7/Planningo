c6c3419d3099e45843a79dd2f9aae8f570aaf89d
tabatabb7 <yoosallyh@gmail.com>
2021-04-28 20:36:38 -0400

    TEST: changed database URL

--- server/db/db.js
+++ server/db/db.js
@@ -5,7 +5,7 @@ const databaseName =
   pkg.name + (process.env.NODE_ENV === "test" ? "-test" : "");
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  process.env.DATABASE_URL || `postgres://jkxmqzvxxeqrbp:0c883cd510d62bc3e4dc90fec7914bee618528959ccae92ad8bc2d99e98f6afb@ec2-3-215-57-87.compute-1.amazonaws.com:5432/d4o54q5lovkv2q`,
   {
     logging: false,
   }
