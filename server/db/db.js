09877fa4c4d89fa05cb5d338c948c7a42f70c218
tabatabb7 <yoosallyh@gmail.com>
2021-04-28 20:44:28 -0400

    TEST: addon added and url updated

--- server/db/db.js
+++ server/db/db.js
@@ -5,7 +5,7 @@ const databaseName =
   pkg.name + (process.env.NODE_ENV === "test" ? "-test" : "");
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://jkxmqzvxxeqrbp:0c883cd510d62bc3e4dc90fec7914bee618528959ccae92ad8bc2d99e98f6afb@ec2-3-215-57-87.compute-1.amazonaws.com:5432/d4o54q5lovkv2q`,
+  process.env.DATABASE_URL || `postgres://znncnmtnyxergd:bbe0dd5655788416de5a3336264ce726ac042fce862129b80be4d4fc84d5f425@ec2-34-225-167-77.compute-1.amazonaws.com:5432/d6b7n50tns7hms`,
   {
     logging: false,
   }
